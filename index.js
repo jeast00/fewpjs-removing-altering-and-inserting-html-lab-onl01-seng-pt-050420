229fbb79cbe6547f29d0928ea1c5938ccbe8e931
Jonathan East <jonathan_east@ymail.com>
2020-11-07 06:21:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // Write your code here!
 main.remove();
 const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.HTML = "Flatiron School is the champion";
+newHeader.className = 'victory';
